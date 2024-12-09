f9b9d2fc41b01756b6bef42ba2897157e2fb87d0
osmaldym <osmaldy11checo@gmail.com>
2024-12-09 02:33:43 -0400

    Updating theme with paper/navigator specs and setting light mode as default

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,20 +1,37 @@
-import { MD3DarkTheme, MD3LightTheme, MD3Theme } from "react-native-paper";
-import { useColorScheme } from "react-native";
+import { MD3DarkTheme, MD3LightTheme, MD3Theme, adaptNavigationTheme } from "react-native-paper";
+import { DefaultTheme as RNDefaultTheme, DarkTheme as RNDarkTheme } from "@react-navigation/native";
 
-const lightTheme = {
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+    reactNavigationDark: RNDarkTheme,
+    reactNavigationLight: RNDefaultTheme
+})
+
+export const AppDefTheme = {
+    ...LightTheme,
     ...MD3LightTheme,
     colors: {
+        ...LightTheme.colors,
         ...MD3LightTheme.colors,
-        primary: "#FF9D00",
+        primary: "#FF9D00"
     }
 }
 
-const darkTheme = {
+export const AppDarkTheme = {
+    ...DarkTheme,
     ...MD3DarkTheme,
     colors: {
+        ...DarkTheme.colors,
         ...MD3DarkTheme.colors,
-        primary: "#FF9D00",
+        primary: "#FF9D00"
     }
 }
 
-export const Theme: MD3Theme = useColorScheme() == 'dark' ? darkTheme : lightTheme;
+/**
+ * Only sets a specific mode because setting dark/light mode 
+ * generates an error and possibly bugs, in a future I need 
+ * to use "state" and "context" to use the app with different
+ * modes automatically and manually (By dark/light mode button)
+ * cause I'm using "navigation", so now I decided only use the
+ * light mode
+ */
+export const Theme: MD3Theme = AppDefTheme;
