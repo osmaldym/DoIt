dc92ef2e32b11b4156e9098f58742f755d079e2d
osmaldym <osmaldy11checo@gmail.com>
2024-12-20 04:14:11 -0400

    Adding new necessary prop onPress

--- components/button.tsx
+++ components/button.tsx
@@ -1,12 +1,13 @@
 import { Button } from "react-native-paper";
 import { PropsWithChildren } from "react";
-import { ViewStyle } from "react-native";
+import { GestureResponderEvent, ViewStyle } from "react-native";
 import { Theme } from "../theme/colors";
 
 type ButtonProps = PropsWithChildren<{
     title?: string,
     textColor?: string,
     buttonColor?: string,
+    onPress?: (e: GestureResponderEvent) => void,
     style?: ViewStyle,
 }>;
 
@@ -15,6 +16,7 @@ export function Btn(props: ButtonProps): React.JSX.Element {
         <Button
             textColor={props.textColor ?? "#000"}
             mode="contained"
+            onPress={props.onPress}
             buttonColor={props.buttonColor ?? Theme.colors.primary}
             style={[props.style]}
         >
