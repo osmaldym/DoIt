8fe435162dea42f0a8aa2902b35175131de5a809
osmaldym <osmaldy11checo@gmail.com>
2024-12-20 04:08:23 -0400

    Instantiating new MMKV

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { PaperProvider } from 'react-native-paper';
 
 import { AppNav } from './navigation/rootStackNavigator';
 import { Theme } from './theme/colors';
+import { MMKV } from 'react-native-mmkv';
+
+export const AppStorage = new MMKV();
 
 function App(): React.JSX.Element {
   return (
