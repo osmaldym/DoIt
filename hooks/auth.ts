ad179c32c225f8732de6a5c97cb13f1cadbc04df
osmaldym <osmaldy11checo@gmail.com>
2024-12-21 18:49:35 -0400

    Moving/Creating all auth methods (login/signin/logout/deleteAccount) in a new file

---
+++ hooks/auth.ts
@@ -0,0 +1,47 @@
+import { AppStorage } from "../App";
+import { DoItApi } from "../api/DoIt";
+import { LoginModel, TokenModel } from "../api/models/login";
+import Api from "../enums/api.enum";
+import { StorageKey } from "../enums/storage.enum";
+
+export async function login(user: LoginModel) {
+    const res = await DoItApi.post(Api.logIn, user);
+    
+    if (res.error) {
+        console.error(res.error);
+        return undefined;
+    }
+
+    const resData: TokenModel = res.data as TokenModel;
+    AppStorage.set(StorageKey.access_token, resData.access_token);
+    return resData.access_token;
+}
+
+export async function signin(user: LoginModel) {
+    const res = await DoItApi.post(Api.signIn, user);
+
+    if (res.error) {
+        console.error(res.error);
+        return undefined;
+    }
+
+    const resData: TokenModel = res.data as TokenModel;
+    AppStorage.set(StorageKey.access_token, resData.access_token);
+    return resData.access_token;
+}
+
+export async function logout() {
+    AppStorage.delete(StorageKey.access_token);
+    return "";
+}
+
+export async function deleteAccount() {
+    const res = await DoItApi.delete(Api.profile);
+    
+    if (res.error) {
+        console.error(res.error);
+        return undefined;
+    }
+
+    return logout();
+}
