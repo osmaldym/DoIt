2b79044a05e4a134a88c8c6c514815c04b85c0fa
osmaldym <osmaldy11checo@gmail.com>
2024-12-21 18:51:03 -0400

    Adding AuthGuard to project

--- navigation/rootStackNavigator.tsx
+++ navigation/rootStackNavigator.tsx
@@ -4,6 +4,9 @@ import { LogInScreen } from "../screens/login";
 import { SignInScreen } from "../screens/signin";
 import { PropsWithRef } from "react";
 import AppRoutes from "../enums/routes.enum";
+import { AuthContext, useAuthGuard } from "../hooks/auth.guard";
+import { HomeScreen } from "../screens/home";
+import { SplashScreen } from "../screens/splash";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,12 +19,32 @@ const opts: NativeStackNavigationOptions = {
 }
 
 export function AppNav(props: AppNavProps): React.JSX.Element {
+    const [guard, authContext] = useAuthGuard();
+
     return (
-        <NavigationContainer theme={props.theme}>
-            <Stack.Navigator screenOptions={opts}>
-                <Stack.Screen name={AppRoutes.logIn} component={LogInScreen} />
-                <Stack.Screen name={AppRoutes.signIn} component={SignInScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <AuthContext.Provider value={authContext}>
+            <NavigationContainer theme={props.theme}>
+                <Stack.Navigator screenOptions={opts}>
+                    {
+                        guard.loading ? (
+                            <Stack.Screen name={AppRoutes.splashScreen} component={SplashScreen} />
+                        ) : !guard.userToken ? (
+                            <>
+                                <Stack.Screen 
+                                    name={AppRoutes.logIn}
+                                    component={LogInScreen}
+                                    options={{
+                                        animationTypeForReplace: guard.logout ? 'pop' : 'push',
+                                    }}
+                                />
+                                <Stack.Screen name={AppRoutes.signIn} component={SignInScreen} />
+                            </>
+                        ) : (
+                            <Stack.Screen name={AppRoutes.home} component={HomeScreen} />
+                        )
+                    }
+                </Stack.Navigator>
+            </NavigationContainer>
+        </AuthContext.Provider>
     )
 }
