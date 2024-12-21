b83ed5098ff9ee4ad2d9f267696adaacff683392
osmaldym <osmaldy11checo@gmail.com>
2024-12-21 19:13:24 -0400

    Adding new password confirmation input, validating it and user can sign in

--- screens/signin.tsx
+++ screens/signin.tsx
@@ -1,10 +1,13 @@
-import { Image, ImageStyle, SafeAreaView, StyleSheet, ViewStyle } from "react-native";
+import { Image, SafeAreaView, StyleSheet } from "react-native";
 import { Appbar } from "react-native-paper";
 import { Input } from "../components/input";
 import { Btn } from "../components/button";
 import { Txt } from "../components/text";
 import { Column } from "../components/arrangements";
 import { useNavigation } from "@react-navigation/native";
+import { useContext, useState } from "react";
+import { AuthContext } from "../hooks/auth.guard";
+import { LoginModel } from "../api/models/login";
 
 const LOGO = require('../assets/logo.png')
 
@@ -24,6 +27,9 @@ const style = StyleSheet.create({
 
 export function SignInScreen(): React.JSX.Element {
     const nav = useNavigation();
+    const { signin: signin }: AuthContext = useContext(AuthContext) as AuthContext;
+    const [user, setUser] = useState({} as LoginModel);
+    const [passForConfirm, setPassForConfirm] = useState('');
 
     return (
         <SafeAreaView>
@@ -37,13 +43,33 @@ export function SignInScreen(): React.JSX.Element {
                     <Txt>Add your data to sign in.</Txt>
                 </Column>
                 <Column>
-                    <Input type="email" />
-                    <Input type="password" label="Password"/>
+                    <Input type="email" onTxtChange={(txt) => user.email = txt} />
+                    <Input type="password" label="Password" onTxtChange={(txt) => user.password = txt}/>
+                    <Input type="password" label="Confirm password" onTxtChange={(txt) => setPassForConfirm(txt)}/>
                 </Column>
                 <Column>
-                    <Btn />
+                    <Btn title="Sign in" onPress={async () => {
+                        const validated: boolean = validate(user, { confirmPass: passForConfirm });
+                        if (!validated) return;
+                        await signin(user);
+                    }} />
                 </Column>
             </Column>
         </SafeAreaView>
     );
+}
+
+type Validations = {
+    confirmPass: string,
+}
+
+function validate(user: LoginModel, validations: Validations): boolean {
+    let isAllRight: boolean = true;
+
+    if (validations.confirmPass != user.password) {
+        console.error("The passwords are different");
+        isAllRight = false;
+    }
+    
+    return isAllRight;
 }
