676217e889d4afdb3c7fcce1a3218b2f31bb2dc3
osmaldym <osmaldy11checo@gmail.com>
2024-12-25 23:24:59 -0400

    Fixing bug setting headers, making it func and adding headers in delete

--- utils/http.ts
+++ utils/http.ts
@@ -2,24 +2,24 @@ import { AppStorage } from "../App";
 import { StorageKey } from "../enums/storage.enum";
 
 export default class HTTP {
-    static headers: HeadersInit_ = {
+    static getHeaders: () => HeadersInit_ = () => ({
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + AppStorage.getString(StorageKey.access_token) ?? "",
-    }
+        "Authorization": "Bearer " + AppStorage.getString(StorageKey.access_token) ?? "",  
+    })
 
     static async get(url: string): Promise<any> {
-        return await fetch(url, { headers: this.headers });
+        return await fetch(url, { headers: this.getHeaders() });
     }
 
     static async post(url: string, data: object): Promise<Response> {
-        return await fetch(url, { method: 'POST', body: JSON.stringify(data), headers: this.headers });
+        return await fetch(url, { method: 'POST', body: JSON.stringify(data), headers: this.getHeaders() });
     }
 
     static async patch(url: string, data: object): Promise<Response> {
-        return await fetch(url, { method: 'PATCH', body: JSON.stringify(data), headers: this.headers });
+        return await fetch(url, { method: 'PATCH', body: JSON.stringify(data), headers: this.getHeaders() });
     }
 
     static async delete(url: string): Promise<Response> {
-        return await fetch(url, { method: 'DELETE' });
+        return await fetch(url, { method: 'DELETE', headers: this.getHeaders() });
     }
 }
